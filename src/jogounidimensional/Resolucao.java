b1939e9951df3e6d4e04986c8f2c6a976c4981c7
Kleber Vales <ksvales@outlook.pt>
2024-10-10 09:33:36 -0300

    Classe main

---
+++ src/jogounidimensional/Resolucao.java
@@ -0,0 +1,25 @@
+package jogounidimensional;
+
+import java.util.*;
+
+public class Resolucao {
+
+   
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+            
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+
+            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+        }
+        scan.close();
+    }
+}
